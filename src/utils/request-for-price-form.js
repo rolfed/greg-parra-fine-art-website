2df1f581289b7e7cb47aef0f05c20da1a568835e
Danniel Rolfe <talkto@dannielrolfe.com>
2024-11-01 22:22:00 -0700

    Add test

--- src/utils/request-for-price-form.js
+++ src/utils/request-for-price-form.js
@@ -8,25 +8,23 @@ export const addQueryParamToForm = (function() {
     const _title = _getQueryParam(_currentUrl, 't');
     const _size = _getQueryParam(_currentUrl, 's');
 
-    const updateTextArea = (title, size) => {
+    const updateTextArea = () => {
         // Find the textarea element
         const textarea = document.querySelector("#textarea-yui_3_17_2_1_1555014059115_8410-field");
 
-        console.log(textarea);
         if (textarea) {
-            console.log('observer');
+            console.log('found textarea', textarea);
             // Set the initial value in the textarea
             const initialText = `Title: ${_title}\nSize: ${_size}`;
             textarea.value = initialText;
 
             // Set up a MutationObserver to monitor changes to the textarea
             const observer = new MutationObserver((mutations) => {
+                console.log(mutations);
                 mutations.forEach((mutation) => {
                     if (mutation.type === "attributes" && mutation.attributeName === "value") {
-                            console.log('change');
                         // If the value gets cleared, reset it
                         if (!textarea.value) {
-                            console.log('cleared');
                             textarea.value = initialText;
                         }
                     }
