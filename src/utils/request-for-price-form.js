2de5460f586923e55c005171658ac9a997868122
Danniel Rolfe <talkto@dannielrolfe.com>
2024-11-01 21:11:22 -0700

    Add inquire handler

--- src/utils/request-for-price-form.js
+++ src/utils/request-for-price-form.js
@@ -1,2 +1,30 @@
-export const requestForPriceForm = (function() {
+export const addQueryParamToForm = (function() {
+    const _getQueryParam = (url, param) => {
+        const urlObj = new URL(url); // Create a URL object
+        return urlObj.searchParams.get(param); // Get the value of the specified query parameter
+    }
+    const _currentUrl = window.location.href;
+    const _getParamFromCurrentUrl = _getQueryParam(_currentUrl);
+    const _title = _getParamFromCurrentUrl(_currentUrl, 't');
+    const _size = _getParamFromCurrentUrl(_currentUrl, 's');
+
+    const updateTextArea = (title, size) => {
+        const textarea = document.querySelector("#textarea-yui_3_17_2_1_1555014059115_8410-field");
+
+        // Check if the textarea exists
+        if (textarea) {
+            // Format the message to display in the textarea
+            const message = `Title: ${title}\nSize: ${size}`;
+
+            // Set the message and disable the textarea
+            textarea.value = message;
+            textarea.disabled = true;
+        }
+    }
+
+    const init = () => {
+        updateTextArea(_title, _size)
+    }
+
+    return init();
 });
