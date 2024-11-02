fe7a06111fe26c08584883facca77a21295ab07d
Danniel Rolfe <talkto@dannielrolfe.com>
2024-11-01 22:00:30 -0700

    Form fix

--- src/utils/request-for-price-form.js
+++ src/utils/request-for-price-form.js
@@ -5,9 +5,8 @@ export const addQueryParamToForm = (function() {
         return urlObj.searchParams.get(param); // Get the value of the specified query parameter
     }
     const _currentUrl = window.location.href;
-    const _getParamFromCurrentUrl = _getQueryParam(_currentUrl);
-    const _title = _getParamFromCurrentUrl(_currentUrl, 't');
-    const _size = _getParamFromCurrentUrl(_currentUrl, 's');
+    const _title = _getQueryParam(_currentUrl, 't');
+    const _size = _getQueryParam(_currentUrl, 's');
 
     const updateTextArea = (title, size) => {
         const textarea = document.querySelector("#textarea-yui_3_17_2_1_1555014059115_8410-field");
@@ -15,11 +14,11 @@ export const addQueryParamToForm = (function() {
         // Check if the textarea exists
         if (textarea) {
             // Format the message to display in the textarea
-            const message = `Title: ${title}\nSize: ${size}`;
+            const message = `Title: ${_title}\nSize: ${_size}`;
 
             // Set the message and disable the textarea
             textarea.value = message;
-            textarea.disabled = true;
+            // textarea.disabled = true;
         }
     }
 
